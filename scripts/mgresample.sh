dbcd5a9d500e78c906406a1d26228f592575bc51
Alexander Refsum Jensenius <arjensenius@gmail.com>
2020-03-01 16:59:01 +0100

    New script

---
+++ scripts/mgresample.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# Description:
+#     Will add all tiff files in folder into one combined image
+#
+# Usage:
+#     Place in folder with video files
+#     Make executable: chmod u+x [scriptname]
+#     Run script: sh [scriptname]
+#
+# Dependency:
+#      Uses ImageMagick
+#
+# Author:
+#     Alexander Refsum Jensenius
+#     University of Oslo
+#
+# Version:
+#     0.1 - 2020-03-01
+# ------------------------------------------------------------------
+
+
+if [ -z "$1" ]; then
+    echo "usage: ./mghistory.sh VIDEO [FRAMES=30]"
+    exit
+fi
+
+FILENAME=$1
+NAMESTRING=`echo $FILENAME | cut -d'.' -f1`;
+
+FRAMES=$2
+
+ffmpeg -i $FILENAME -filter:v "setpts=PTS/$FRAMES" -an ${NAMESTRING}_resample.mp4
